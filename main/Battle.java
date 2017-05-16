dee8663ae71e9cd567036d606d1b493006b34eba
ADSI <ADSI@ADSI-A3>
2017-05-15 21:31:17 -0300

    classe batalha com o metodo atack ja feito para debate

--- main/Battle.java
+++ main/Battle.java
@@ -1,5 +1,31 @@
 package main;
 
 public class Battle {
-	private int level;
+	
+	public void battle(Player caracter,Player enemy){
+		private boolean turn;
+		private int buffer;// variavel responsavel por guardar os cauculos
+		caracter.life = 100;
+		enemy.life = 100;
+		do{
+			if (caracter.turnAction == "a"){
+				
+				public int attack(int attack, int life, String turnAction, int defense){//pega o ataque a vida e o que o inimigo escolheu
+					if (turn == true){
+						 if (enemy.turnAction == "d"){
+							 buffer = caracter.attack - enemy.defense;
+							 enemy.life -= buffer;
+						 }else
+							 enemy.life -= caracter.attack;
+					}else{
+						 if (caracter.turnAction == "d"){
+							 buffer = enemy.attack - caracter.defense;
+							 caracter.life -= buffer;
+						 }else
+							 caracter.life -= enemy.attack;	
+					}
+				}
+			}
+		}while(caracter.life > 0 && enemy.life > 0)//verifica se alguem ja morreu
+	}
 }

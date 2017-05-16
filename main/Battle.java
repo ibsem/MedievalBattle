a3cdfebfc5789fe0da324563c2e324abe8d5d082
ADSI <ADSI@ADSI-A3>
2017-05-16 18:31:12 -0300

    Estou enviando uma parte do codigo referente a escolha, um breve visao para saber se estou no caminho certo.

--- main/Battle.java
+++ main/Battle.java
@@ -1,30 +1,59 @@
+import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 package main;
 
 public class Battle {
 	
-	public void battle(Player caracter,Player enemy){
-		private boolean turn;
+	public void battle(Player caracter,Player enemy)throws IOException {
+		private boolean turn;// 'true' vez do jogador , 'false' vez do inimigo
 		private int buffer;// variavel responsavel por guardar os cauculos
-		caracter.life = 100;
-		enemy.life = 100;
+		caracter.setLife = 100;
+		enemy.setLife = 100;
 		do{
-			if (caracter.turnAction == "a"){
+			
+			public int choseAction(Player caracter,Player enemy){
+
+				System.out.println("Escolha sua Acao ");//permite a escolha da acao
+				System.out.println("1 - Attack");
+				System.out.println("2 - Defense");
+				System.out.println("3 - Special");
+				BufferedReader chose = new BufferedReader(new InputStreamReader(System.in));
+				int chosePlayer = chose.readLine();				
 				
-				public int attack(int attack, int life, String turnAction, int defense){//pega o ataque a vida e o que o inimigo escolheu
-					if (turn == true){
-						 if (enemy.turnAction == "d"){
-							 buffer = caracter.attack - enemy.defense;
-							 enemy.life -= buffer;
-						 }else
-							 enemy.life -= caracter.attack;
-					}else{
-						 if (caracter.turnAction == "d"){
-							 buffer = enemy.attack - caracter.defense;
-							 caracter.life -= buffer;
-						 }else
-							 caracter.life -= enemy.attack;	
-					}
+				Random random = new Random();
+				int choseEnemy = ran.nextInt(3);//escolhe a acao do inimigo
+				
+				switch(chosePlayer){ // permite ao usuario escolher a acao
+					case 1 :
+						caracter.setTurnAction = 'a';
+						break;
+					case 2 :
+						caracter.setTurnAction = 'd';
+						break;
+					case 3 :
+						caracter.setTurnAction = 's';
+						break;
+					default :
+						System.out.println("Invalid Moviment");
 				}
+				
+				switch(choseEnemy){ // marca a acao do inimigo 
+					case 0 :
+						enemy.setTurnAction = 'a';
+						break;
+					case 1 :
+						enemy.setTurnAction = 'd';
+						break;
+					case 2 :
+						enemy.setTurnAction = 's';
+						break;
+					default :
+						System.out.println("Invalid Moviment");
+				}
+				
 			}
 		}while(caracter.life > 0 && enemy.life > 0)//verifica se alguem ja morreu
 	}

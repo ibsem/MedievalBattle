294e0c4525ffcf76f581d88eda8a1fb435ed571e
ADSI <ADSI@ADSI-A1>
2017-05-29 20:10:55 -0300

    First base of the Class Game.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,15 +1,20 @@
-
 public class Game {
 	public static void main(String []args){
-	  SqliteConnection conn = new SqliteConnection();
-	  conn.connect();
-	  conn.initDatabase();
+	  //SqliteConnection conn = new SqliteConnection();
+ 	  //conn.connect();
+	  //conn.initDatabase();
+		
+		CharacterCreation player = new CharacterCreation();		
+		SqliteConnection sqliteConnection1 = new SqliteConnection();
+		Player p1 = new Player("Jogador", null, 100, 3, 2, 3);
+		Player p2 = new Player("Inimigo", null, 100, 1, 2, 1);
+		Battle battle = new Battle();
+		String winner = battle.battle(p1, p2);
+		
+		System.out.println("The " + winner + "ganhou!");		
 	}
 	
 	public void initDatbase(){
 		
 	}
-	
-	
-
 }

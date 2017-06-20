8e9776d9244109aafddaebab1f230f29ec649e83
ADSI <ADSI@ADSI-A3>
2017-06-19 21:44:31 -0300

    documento sobre diagrama de classes e casos de uso

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,13 +2,13 @@ import java.io.IOException;
 
 public class Game {
 	public static void main(String []args)throws IOException{
-	  //SqliteConnection conn = new SqliteConnection();
- 	  //conn.connect();
-	  //conn.initDatabase();
+	  SqliteConnection conn = new SqliteConnection();
+ 	  conn.connect();
+	  conn.initDatabase();
 		
-		//CharacterCreation player = new CharacterCreation();		
-		//SqliteConnection sqliteConnection1 = new SqliteConnection();
-		Player p1 = new Player("Jogador", null, 100, 3, 2, 3);
+		CharacterCreation player = new CharacterCreation();		
+		SqliteConnection sqliteConnection1 = new SqliteConnection();
+		Player p1 = new Player("Player", null, 100, 3, 2, 3);
 		Player p2 = new Player("Inimigo", null, 100, 1, 2, 1);
 		Battle battle = new Battle();
 		String winner = battle.startBattle(p1, p2);

a9f78b95eaf8a3521d54ad0c40952c39b64793a7
ADSI <ADSI@ADSI-A3>
2017-06-23 20:09:15 -0300

    Estrutura de ranking pronta para testes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,13 @@
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 public class Game {
 	public static void main(String []args)throws IOException{
@@ -13,10 +22,30 @@ public class Game {
 		Battle battle = new Battle();
 		String winner = battle.startBattle(p1, p2);
 		
-		System.out.println("O " + winner + " ganhou!");		
+		System.out.println("O " + winner + " ganhou!");
+		ranking();
 	}
 	
 	public void initDatbase(){
+		
+	}
+	
+	public static void ranking(){
+		SqliteConnection sqlite = new SqliteConnection();
+		String sql = "SELECT * FROM ranking ORDER BY score DESC LIMIT 10;";
+		System.out.println("O Ranking dos 10 Melhores");
+		try (Connection conn = sqlite.connect();
+				Statement stmt  = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);){
+			while (rs.next()){
+				String name = rs.getObject(2).toString();
+				String score =  rs.getObject(3).toString();
+				String data = rs.getObject(4).toString();
+				System.out.println("Jogador: " + name + " com " + score + " pontos, na data: " + data);
+			}
+		}catch (SQLException e) {
+			System.out.println("Erro ->"+e);
+		}
 		
 	}
 }
